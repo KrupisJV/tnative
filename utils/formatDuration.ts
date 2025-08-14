f7568b7e10970037054fef857fc32a50aa7737fb
KrupisJV <toadgod123@gmail.com>
2025-08-14 21:25:36 +0300

    Initial commit

---
+++ utils/formatDuration.ts
@@ -0,0 +1,6 @@
+export function formatDuration(totalSeconds: number) {
+    const m = Math.floor(totalSeconds / 60);
+    const s = totalSeconds % 60;
+    return `${m}:${s.toString().padStart(2, "0")}`;
+  }
+  
